fd01c50cd2101f865c070c43de1063f3feae0e39
zcfrank1st <zcfrank1st@sina.com>
2016-11-28 15:15:45 +0800

    hades-extension first commit

---
+++ devops/src/app/project.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ProjectService {
+  private username = window.localStorage.getItem("USER-NAME");
+  private pattern;
+
+  constructor() {
+    this.pattern = new RegExp("(http|https)://.*" +"/(" + this.username + ")/(\\w+)/?.*");
+  }
+
+  isProjectPage (path: string) {
+    return this.pattern.test(path);
+  }
+
+  projectName (path: string) {
+    return path.match(this.pattern)[3];
+  }
+
+  projectPath (path: string) {
+    return path.match(this.pattern)[2] + "/" + path.match(this.pattern)[3];
+  }
+
+  currentUrl () {
+    return new Promise<string> ((resolve) => {
+      chrome.tabs.query({active: true}, (tabs) => {
+        resolve(tabs[0].url);
+      });
+    });
+  }
+}
